f937f722e2ffbf19d97eac247aa4aec8fc3b7001
SirLemoniada <igor.dmochowski@gmail.com>
2020-03-21 23:47:52 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'RSSDemo/RSSDemo.dart';
+import 'package:expandable/expandable.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 10,
+        navigateAfterSeconds: new AfterSplash(),
+        title: new Text('Welcome to Corominder!',
+          style: new TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0
+          ),),
+        image: new Image.network('https://prudensai.pl/corominder.png'),
+        backgroundColor: Colors.grey,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        onClick: ()=>print("Wash your hands!"),
+        loaderColor: Colors.blue
+    );
+  }
+}
+
+class MyItem{
+  MyItem({this.isExpanded: false, this.header, this.body});
+  bool isExpanded;
+  final String header;
+  final String body;
+
+}
+
+
+class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'RSS',
+      home: new RSSDemo(),
+    );
+  }
+
+}
